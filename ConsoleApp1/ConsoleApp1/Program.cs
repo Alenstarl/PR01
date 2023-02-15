cdf2ca4f28e45b06ac0362c357b92eff5875b2ff
Alenstarl <124236667+Alenstarl@users.noreply.github.com>
2023-02-15 19:19:20 +0700

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+class AnagramChecker
+{
+    static void Main(string[] args)
+    {
+        string s = "silent";
+        string t = "listen";
+        bool result = IsAnagram(s, t);
+        Console.WriteLine(result); 
+    }
+
+    static bool IsAnagram(string s, string t)
+    {
+        if (s.Length != t.Length)
+        {
+            return false;
+        }
+
+        char[] sChars = s.ToCharArray();
+        char[] tChars = t.ToCharArray();
+        Array.Sort(sChars);
+        Array.Sort(tChars);
+        string sortedS = new string(sChars);
+        string sortedT = new string(tChars);
+
+        return sortedS == sortedT;
+    }
+}
